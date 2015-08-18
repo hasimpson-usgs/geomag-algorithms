2c3323629106824627c86adc72ee991427cfa597
Hal Simpson <hasimpson@usgs.gov>
2015-08-18 09:54:59 -0600

    Added IAFParser_test framework

---
+++ geomagio/iaf/IAFParser_test.py
@@ -0,0 +1,10 @@
+"""Tests for the IAF Parser class."""
+
+from nose.tools import assert_equals
+from IAFParser import IAFParser
+
+
+def test__merge_comments():
+    """geomagio.iaf.IAFParser_test.test_parse()
+    """
+    assert_equals(True, True)
