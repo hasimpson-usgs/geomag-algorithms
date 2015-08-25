424e33986e0893036eb18b759d32b467cbeddb01
Hal Simpson <hasimpson@usgs.gov>
2015-08-25 11:17:05 -0600

    Reordered function to be alphabetical

--- geomagio/iaf/IAFWriter.py
+++ geomagio/iaf/IAFWriter.py
@@ -63,27 +63,6 @@ class IAFWriter(object):
             self._fill_data(timeseries, channels, starttime, day)
             self._write(out)
 
-    def _get_channels(self, channels, timeseries):
-        """get channels
-        Parameters
-        ----------
-        channels : array_like
-            array of channels passed in from controller
-        timeseries : obspy.core.Stream
-            timeseries data
-        Returns
-        -------
-        array_like
-            an array of channels
-        """
-        if channels is not None:
-            return channels
-        elif len(timeseries.select(channel='H')) > 0:
-            return H_CHANNELS
-        elif len(timeseries.select(channel='X')) > 0:
-            return X_CHANNELS
-        return None
-
     def _check_month(self, starttime, endtime):
         """Make certain time frame is a month
 
@@ -106,106 +85,26 @@ class IAFWriter(object):
                     'Time frame does not cover a full month "%s" "%s"' %
                             (starttime, endtime))
 
-    def _write(self, out):
-        """ write record to file
-
+    def _get_channels(self, channels, timeseries):
+        """get channels
         Parameters
         ----------
-        out : output pipe
-            pre-opened write output pipe
-        """
-        record = struct.pack(IAFParser.IAFSTRING,
-            self.headers['station'],
-            self.headers['date'],
-            self.headers['geodetic_latitude'],
-            self.headers['geodetic_longitude'],
-            self.headers['elevation'],
-            self.headers['channels'],
-            self.headers['agency_name'],
-            self.headers['d_conversion'],
-            self.headers['data_quality'],
-            self.headers['instrumentation'],
-            self.headers['k_9'],
-            self.headers['sensor_sampling_rate'],
-            self.headers['sensor_orientation'],
-            self.headers['publication_date'],
-            self.headers['version'],
-            0,
-            *self.data)
-        out.write(record)
-
-    def _fill_headers(self, stats, channels=None, starttime=None):
-        """format headers for IAFV283 file
-
-        Parameters
-        ----------
-        stats : obspy.core.trace.stats
-            holds the observatory metadata
         channels : array_like
-            channels to be reported.
-        starttime : obspy.core.UTCDateTime
-            starttime of the data
-        """
-        self.headers['station'] = stats['station'].rjust(4)
-        self.headers['date'] = self._get_record_date(starttime)
-        self.headers['geodetic_latitude'] = \
-                int(float(stats['geodetic_latitude']) * 1000)
-        self.headers['geodetic_longitude'] = \
-                int(float(stats['geodetic_longitude']) * 1000)
-        self.headers['elevation'] = int(stats['elevation'])
-        self.headers['channels'] = self._get_channels_headers(
-                channels, stats)
-        self.headers['agency_name'] = stats['agency_name']
-        self.headers['d_conversion'] = stats['d_conversion']
-        self.headers['data_quality'] = stats['data_quality']
-        self.headers['instrumentation'] = stats['instrumentation']
-        self.headers['k_9'] = stats['k_9']
-        self.headers['sensor_sampling_rate'] = \
-                int(float(stats['sensor_sampling_rate']) * 1000.0)
-        self.headers['sensor_orientation'] = stats['sensor_orientation']
-        self.headers['publication_date'] = self._get_publication_date()
-        self.headers['version'] = self._get_version(stats['version'])
-        self.headers['empty'] = 0
-
-    def _get_record_date (self, starttime):
-        """get record data
-
-        Parameters
-        ----------
-        starttime : obspy.core.UTCDateTime
-            starttime of data
-
+            array of channels passed in from controller
+        timeseries : obspy.core.Stream
+            timeseries data
         Returns
         -------
-        int
-            The year and day of year as an int in the format YYYYddd
+        array_like
+            an array of channels
         """
-        return int('%04d%03d' % (starttime.year, starttime.day))
-
-    def _get_publication_date(self):
-        """get publication date
-
-        Returns
-        -------
-        str
-            The current date as a string formated as the year and month YYmmm
-        """
-        year = int(('%04d' % self.now.year)[2:4]) * 100
-        month = self.now.month
-        return str(year + month)
-
-    def _get_version(self, version):
-        """get version
-
-        Parameters
-        ----------
-        version : int
-            version of the file
-        Returns
-        -------
-            version of the file
-        """
-        return version or self.version
+        if channels is not None:
+            return channels
+        elif len(timeseries.select(channel='H')) > 0:
+            return H_CHANNELS
+        elif len(timeseries.select(channel='X')) > 0:
+            return X_CHANNELS
+        return None
 
     def _get_channels_headers(self, channels, stats):
         """get channels headers
@@ -229,6 +128,46 @@ class IAFWriter(object):
                 return ''.join(channels)
             else:
                 raise TimeseriesFactoryException('No channels found')
+
+    def _get_publication_date(self):
+        """get publication date
+
+        Returns
+        -------
+        str
+            The current date as a string formated as the year and month YYmmm
+        """
+        year = int(('%04d' % self.now.year)[2:4]) * 100
+        month = self.now.month
+        return str(year + month)
+
+    def _get_record_date (self, starttime):
+        """get record data
+
+        Parameters
+        ----------
+        starttime : obspy.core.UTCDateTime
+            starttime of data
+
+        Returns
+        -------
+        int
+            The year and day of year as an int in the format YYYYddd
+        """
+        return int('%04d%03d' % (starttime.year, starttime.day))
+
+    def _get_version(self, version):
+        """get version
+
+        Parameters
+        ----------
+        version : int
+            version of the file
+        Returns
+        -------
+            version of the file
+        """
+        return version or self.version
 
     def _fill_data(self, timeseries, channels, starttime, day):
         """
@@ -293,6 +232,39 @@ class IAFWriter(object):
         self.data = numpy.append(self.data, data)
         self.data = numpy.append(self.data, [0,0,0,0])
 
+    def _fill_headers(self, stats, channels=None, starttime=None):
+        """format headers for IAFV283 file
+
+        Parameters
+        ----------
+        stats : obspy.core.trace.stats
+            holds the observatory metadata
+        channels : array_like
+            channels to be reported.
+        starttime : obspy.core.UTCDateTime
+            starttime of the data
+        """
+        self.headers['station'] = stats['station'].rjust(4)
+        self.headers['date'] = self._get_record_date(starttime)
+        self.headers['geodetic_latitude'] = \
+                int(float(stats['geodetic_latitude']) * 1000)
+        self.headers['geodetic_longitude'] = \
+                int(float(stats['geodetic_longitude']) * 1000)
+        self.headers['elevation'] = int(stats['elevation'])
+        self.headers['channels'] = self._get_channels_headers(
+                channels, stats)
+        self.headers['agency_name'] = stats['agency_name']
+        self.headers['d_conversion'] = stats['d_conversion']
+        self.headers['data_quality'] = stats['data_quality']
+        self.headers['instrumentation'] = stats['instrumentation']
+        self.headers['k_9'] = stats['k_9']
+        self.headers['sensor_sampling_rate'] = \
+                int(float(stats['sensor_sampling_rate']) * 1000.0)
+        self.headers['sensor_orientation'] = stats['sensor_orientation']
+        self.headers['publication_date'] = self._get_publication_date()
+        self.headers['version'] = self._get_version(stats['version'])
+        self.headers['empty'] = 0
+
     def _get_slice(self, timeseries, starttime, day):
         """get slice
         timeseries : obspy.core.stream
@@ -305,3 +277,31 @@ class IAFWriter(object):
         start = starttime + day * 86400.0
         end = start + 86340.0
         return timeseries.slice(start, end)
+
+    def _write(self, out):
+        """ write record to file
+
+        Parameters
+        ----------
+        out : output pipe
+            pre-opened write output pipe
+        """
+        record = struct.pack(IAFParser.IAFSTRING,
+            self.headers['station'],
+            self.headers['date'],
+            self.headers['geodetic_latitude'],
+            self.headers['geodetic_longitude'],
+            self.headers['elevation'],
+            self.headers['channels'],
+            self.headers['agency_name'],
+            self.headers['d_conversion'],
+            self.headers['data_quality'],
+            self.headers['instrumentation'],
+            self.headers['k_9'],
+            self.headers['sensor_sampling_rate'],
+            self.headers['sensor_orientation'],
+            self.headers['publication_date'],
+            self.headers['version'],
+            0,
+            *self.data)
+        out.write(record)
